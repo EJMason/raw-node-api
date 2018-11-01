abd7da2407b0c795ab87473db0119c1e9103f660
EJ Mason <eliotjunior@gmail.com>
2018-11-01 13:17:38 -0700

    more cleanup, routes now isolated

--- router.js
+++ router.js
@@ -1,21 +1,10 @@
-const url = require('url')
-
 const Server = require('./lib/Server')
 
-const obj = {}
-
-const defaultHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'application/json'
-};
 
 const router = Server.Router()
 
 router.addRoute('GET', '/hello', (req, res) => {
-  res.writeHead(200, defaultHeaders)
+  res.writeHead(200, router.defaultHeaders)
 
   const data = {
     statusCode: 200,
@@ -25,16 +14,4 @@ router.addRoute('GET', '/hello', (req, res) => {
 })
 
 
-obj.requestHandler = (req, res) => {
-  //TODO: Add default headers here using res
-  //TODO: Add middleware?
-
-  const pathname = '/' + url.parse(req.url).pathname.replace(/^\/+|\/+$/g, '')
-  const method = req.method.toUpperCase()
-  console.log(pathname)
-
-  return router.getRequestHandler(method, pathname)
-}
-
-
-module.exports = obj
+module.exports = router
