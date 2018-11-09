39af13e8bdeb74dd115d2c0f9962a9304f99d4b9
EJ Mason <eliotjunior@gmail.com>
2018-11-08 17:38:06 -0800

    [refactor] rebuilt the server class to be more extensible

--- appHttp.js
+++ appHttp.js
@@ -1,9 +1,8 @@
 const config = require('./lib/config')
 const NodeServer = require('./lib/Server')
 
-const app = new NodeServer()
+const app = new NodeServer('http', config.app.httpPort)
 
-app
-  .setPort(config.app.httpPort)
+
 
 module.exports = exports = app
--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-
-
 const app = require('./appHttp')
 
-// const config = {
-//   nodeEnv: 'development',
-//   httpOptions: {
-//     port: 3000,
-//     path: 'localhost'
-//   }
-// };
+app
+  .bootstrap()
+  .start()
 
-app.test()
-
-
-
-// if (httpServer.listening) {
-//   console.log('Server is listening!')
-//   httpServer.on('request', (req, res) => {
-//     if (!res) console.log('no res')
-//     const method = Server.getMethod(req)
-//     const pathname = Server.getPathName(req)
-//     Server.requestLogger(method, pathname)
-//   })
-// }
 
