4731624b52f1a814528dbbe6f778541f1aa8c75f
EJ Mason <eliotjunior@gmail.com>
2019-01-01 17:35:44 -0700

    start point for the menu routes

---
+++ menu/index.js
@@ -0,0 +1,7 @@
+const routes = require('./routes')
+
+
+const menu = exports = module.exports = {}
+
+
+menu.routes = routes
---
+++ menu/routes.js
@@ -0,0 +1,24 @@
+
+const router = exports = module.exports = {}
+
+router['/menu'] = {
+
+  /**
+   * Get a list of items from the menu, (hardcoded)
+   *
+   *
+   *
+   * Required Data:
+   *
+   * Success Returned Data:
+   *
+   * ? Possible Features:
+   *    * Pagination
+   *    * Filtering
+   *    * Sorting
+   */
+  GET: (req, res) => {
+
+  },
+
+}
