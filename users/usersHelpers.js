2aedfabff28fe94568730e73f40b5f289cd8aa1c
EJ Mason <eliotjunior@gmail.com>
2019-01-14 17:09:24 -0700

    Added methods specific to validating user data
    - Added regex checker for password validation
    - Added regex checker for email validation
    - added checker to see if state code passes certain validations and is a valid state

---
+++ users/usersHelpers.js
@@ -0,0 +1,36 @@
+
+
+// Using official standard:
+// https://stackoverflow.com/a/1373724/3109222
+const emailStandard = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+module.exports.isEmailValid = (email) => emailStandard.test(email.toLowerCase())
+
+
+/**
+ * Regex to validate password
+ * (?=.*\d)          // should contain at least one digit
+ * (?=.*[a-z])       // should contain at least one lower case
+ * (?=.*[A-Z])       // should contain at least one upper case
+ * [a-zA-Z0-9]{8,}   // should contain at least 8 from the mentioned characters
+ *
+ */
+module.exports.isPasswordValid = (pass) => {
+  const passwordChecker = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
+
+  return passwordChecker.test(pass)
+
+}
+
+module.exports.stateIsValid = (state) => {
+  const stateAbbreviations = [
+ 'AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA',
+ 'GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA',
+ 'MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND',
+ 'MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT',
+ 'VT','VI','VA','WA','WV','WI','WY'
+]
+
+  if (!(typeof state === 'string')) return false
+  if (state.length !== 2) return false
+  if ((stateAbbreviations.includes(state.toUpperCase())))
+}
