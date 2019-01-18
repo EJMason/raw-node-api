de38b6e8b1ebd10a7981ccb82414e4118f0c603e
EJ Mason <eliotjunior@gmail.com>
2019-01-17 19:22:06 -0700

    Minor changes

--- index.js
+++ index.js
@@ -17,13 +17,3 @@ app
 errorLib.errorHandler()
 
 
-// process.once('SIGINT', () => {
-//   console.log('\n\nSIGINT SIGNAL RECEIVED... goodbye....')
-
-//   if(app.server) {
-//     app.server.close(() => {
-//       console.log('The server has been closed successfully')
-//       process.exit(0)
-//     })
-//   }
-// })
