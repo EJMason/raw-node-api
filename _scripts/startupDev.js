4e930cdc2031c72068d39b2939ce60a463495ac0
EJ Mason <eliotjunior@gmail.com>
2019-01-17 17:15:08 -0700

    Added script for some setup before starting server

---
+++ _scripts/startupDev.js
@@ -0,0 +1,18 @@
+
+
+// check if the .data folder exists, create if not
+
+/**
+ * Check if these directories exists
+ *    * .data/users/
+ *    * .data/menu/
+ *    * .data/tokens/
+ *    * .data/carts/
+ *    * .data/orders/
+ *
+ */
+
+// generate test data and mock data
+
+// ability to clear out the database
+
